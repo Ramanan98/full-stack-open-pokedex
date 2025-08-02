01e8a5312d4cea8aaafdfa940e229b1063997acc
Ramanan <ramanan.murugesan@gmail.com>
2025-08-02 15:04:56 +0530

    ex-11.12

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.use(express.static('dist'))
